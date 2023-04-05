f6b968ec43db8a01804b799532197d72fbb37dc6
ramintasalina <brigita.salina@gmail.com>
2023-04-05 16:00:46 +0300

    Initial commit

---
+++ src/main/java/User.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+
+public class User {
+    public String userName;
+    public String email;
+    public String password;
+    public String password2;
+    public static WebDriver driver;
+
+    public User(String name, String email, String password, String repeatPassword) {
+        this.userName = name;
+        this.email = email;
+        this.password = password;
+        this.password2 = repeatPassword;
+    }
+
+    public static boolean registerUser(User user){
+        driver.get("https://elenta.lt/registracija");
+        driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[1]/div[2]/div[2]/button[1]/p")).click();
+        WebElement userName = driver.findElement(By.id("UserName"));
+        WebElement email = driver.findElement(By.id("Email"));
+        WebElement password = driver.findElement(By.id("Password"));
+        WebElement password2 = driver.findElement(By.id("Password2"));
+        WebElement submit = driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[11]/td[2]/input"));
+
+        userName.sendKeys(user.userName);
+        email.sendKeys(user.email);
+        password.sendKeys(user.password);
+        password2.sendKeys(user.password2);
+        submit.click();
+        return true;
+    }
+
+
+}
---
+++ src/test/java/ElentaRegisterUserTest.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import java.time.Duration;
+
+import  static org.testng.Assert.*;
+
+
+public class ElentaRegisterUserTest {
+
+
+    @Test
+    public void registerUser(){
+        User user = new User("Brigita", "brigita.salina@gmail.com","tralialia","tralialia");
+        assertTrue( User.registerUser(user) );
+    }
+
+
+
+
+
+
+
+    @BeforeClass
+    public void beforeClass(){
+        System.setProperty("webdriver.com.driver","drivers\\chromedriver111.exe");
+        User.driver = new ChromeDriver();
+        User.driver.manage().window().maximize();
+        User.driver. manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    }
+
+    @AfterClass
+    public void afterClass(){
+//        driver.quit();
+    }
+
+
+}
